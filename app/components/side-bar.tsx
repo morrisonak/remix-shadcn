1495ad952304000a46ca67e13dad76c666fd31be
Justin Morrison <57382686+morrisonak@users.noreply.github.com>
2023-12-22 19:33:44 +0000

    update colors

--- app/components/side-bar.tsx
+++ app/components/side-bar.tsx
@@ -6,62 +6,61 @@ import { Package2Icon, HomeIcon, ShoppingCartIcon, PackageIcon, UsersIcon, LineC
 export function SideBar() {
   return (
     <div className="grid h-screen min-h-screen w-full overflow-hidden lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-gray-100/40 lg:block dark:bg-gray-800/40">
-        <div className="flex flex-col gap-2">
-          <div className="flex h-[60px] items-center px-6">
-            <Link className="flex items-center gap-2 font-semibold" to="./">
-              <Package2Icon className="h-6 w-6" />
-              <span>Signal Configuration</span>
+    <div className="hidden border-r bg-gray-50 lg:block dark:bg-gray-200">
+      <div className="flex flex-col gap-2">
+        <div className="flex h-[60px] items-center px-6">
+          <Link className="flex items-center gap-2 font-semibold text-black dark:text-gray-900" to="./">
+            <Package2Icon className="h-6 w-6" />
+            <span>Signal Configuration</span>
+          </Link>
+        </div>
+        <div className="flex-1">
+          <nav className="grid items-start px-4 text-sm font-medium">
+            <Link
+              className="flex items-center gap-3 rounded-lg px-3 py-2 text-black hover:bg-gray-200 dark:text-gray-900 dark:hover:bg-gray-300"
+              to="./"
+            >
+              <HomeIcon className="h-4 w-4" />
+              Home
             </Link>
-          </div>
-          <div className="flex-1">
-            <nav className="grid items-start px-4 text-sm font-medium">
-              <Link
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-                to="./"
-              >
-                <HomeIcon className="h-4 w-4" />
-                Home
-              </Link>
-              <Link
-                className="flex items-center gap-3 rounded-lg bg-gray-100 px-3 py-2 text-gray-900  transition-all hover:text-gray-900 dark:bg-gray-800 dark:text-gray-50 dark:hover:text-gray-50"
-                to="wayside"
-              >
-                <ShoppingCartIcon className="h-4 w-4" />
-                Wayside
-                <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">12</Badge>
-              </Link>
-              <Link
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-                to="crossings"
-              >
-                <PackageIcon className="h-4 w-4" />
-                Crossings
-              </Link>
-              <Link
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-                to="#"
-              >
-                <UsersIcon className="h-4 w-4" />
-                Customers
-              </Link>
-              <Link
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-                to="#"
-              >
-                <LineChartIcon className="h-4 w-4" />
-                Analytics
-              </Link>
-            </nav>
-          </div>
+            <Link
+              className="flex items-center gap-3 rounded-lg bg-gray-50 px-3 py-2 text-black hover:bg-gray-200 dark:bg-gray-200 dark:text-gray-900 dark:hover:bg-gray-300"
+              to="wayside"
+            >
+              <ShoppingCartIcon className="h-4 w-4" />
+              Wayside
+              <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full bg-gray-400 text-white">12</Badge>
+            </Link>
+            <Link
+              className="flex items-center gap-3 rounded-lg px-3 py-2 text-black hover:bg-gray-200 dark:text-gray-900 dark:hover:bg-gray-300"
+              to="crossings"
+            >
+              <PackageIcon className="h-4 w-4" />
+              Crossings
+            </Link>
+            <Link
+              className="flex items-center gap-3 rounded-lg px-3 py-2 text-black hover:bg-gray-200 dark:text-gray-900 dark:hover:bg-gray-300"
+              to="#"
+            >
+              <UsersIcon className="h-4 w-4" />
+              Customers
+            </Link>
+            <Link
+              className="flex items-center gap-3 rounded-lg px-3 py-2 text-black hover:bg-gray-200 dark:text-gray-900 dark:hover:bg-gray-300"
+              to="#"
+            >
+              <LineChartIcon className="h-4 w-4" />
+              Analytics
+            </Link>
+          </nav>
         </div>
       </div>
-      <div className="flex flex-col">
-       
-        <Outlet />
-        
-      </div>
     </div>
+    <div className="flex flex-col">
+      <Outlet />
+    </div>
+  </div>
+  
   )
 }
 
--- app/components/waysideHeader.tsx
+++ app/components/waysideHeader.tsx
@@ -6,53 +6,53 @@ import { DropdownMenuTrigger, DropdownMenuLabel, DropdownMenuSeparator, Dropdown
 
 export function WaysideHeader() {
     return (
+<header className="flex h-14 lg:h-[60px] items-center gap-4 border-b bg-gray-50 px-6 dark:bg-gray-200">
+    <Link className="lg:hidden" to="#">
+        <Package2Icon className="h-6 w-6 text-black dark:text-gray-900" />
+        <span className="sr-only">Home</span>
+    </Link>
+    <div className="flex-1">
+        <h1 className="font-semibold text-lg text-black dark:text-gray-900">Wayside</h1>
+    </div>
+    <div className="flex flex-1 items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+        <form className="ml-auto flex-1 sm:flex-initial">
+            <div className="relative">
+                <SearchIcon className="absolute left-2.5 top-2.5 h-4 w-4 text-gray-500 dark:text-gray-400" />
+                <Input
+                    className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px] bg-white"
+                    placeholder="Search Wayside..."
+                    type="search"
+                />
+            </div>
+        </form>
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button className="rounded-full" size="icon" variant="ghost">
+                    <img
+                        alt="Avatar"
+                        className="rounded-full"
+                        height="32"
+                        src="/placeholder.svg"
+                        style={{
+                            aspectRatio: "32/32",
+                            objectFit: "cover",
+                        }}
+                        width="32"
+                    />
+                    <span className="sr-only">Toggle user menu</span>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Settings</DropdownMenuItem>
+                <DropdownMenuItem>Support</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    </div>
+</header>
 
-        <header className="flex h-14 lg:h-[60px] items-center gap-4 border-b bg-gray-100/40 px-6 dark:bg-gray-800/40">
-            <Link className="lg:hidden" to="#">
-                <Package2Icon className="h-6 w-6" />
-                <span className="sr-only">Home</span>
-            </Link>
-            <div className="flex-1">
-                <h1 className="font-semibold text-lg">Wayside</h1>
-            </div>
-            <div className="flex flex-1 items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-                <form className="ml-auto flex-1 sm:flex-initial">
-                    <div className="relative">
-                        <SearchIcon className="absolute left-2.5 top-2.5 h-4 w-4 text-gray-500 dark:text-gray-400" />
-                        <Input
-                            className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px] bg-white"
-                            placeholder="Search Wayside..."
-                            type="search"
-                        />
-                    </div>
-                </form>
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button className="rounded-full" size="icon" variant="ghost">
-                            <img
-                                alt="Avatar"
-                                className="rounded-full"
-                                height="32"
-                                src="/placeholder.svg"
-                                style={{
-                                    aspectRatio: "32/32",
-                                    objectFit: "cover",
-                                }}
-                                width="32"
-                            />
-                            <span className="sr-only">Toggle user menu</span>
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>Settings</DropdownMenuItem>
-                        <DropdownMenuItem>Support</DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>Logout</DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            </div>
-        </header>
     )
 }
